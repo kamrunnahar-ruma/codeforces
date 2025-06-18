838cf6e93669d337bad97cde696263500f50bdab
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-06-18 13:35:53 +0600

    Add files via upload

---
+++ 1760A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a>b && b>c )||(c>b && b>a)){
+            cout<<b<<endl;
+        }
+        else if((b>a && a>c)||(c>a && a>b)){
+            cout<<a<<endl;
+        }
+        else if((a>c && c>b)||(b>c && c>a)){
+            cout<<c<<endl;
+        }
+    }
+    return 0;
+}
