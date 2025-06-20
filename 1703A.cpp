04153b312cbac4a07c3788305036baf5790f733e
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-06-20 08:30:46 +0600

    Add files via upload

---
+++ 1703A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        for (int i = 0; i < str.size(); ++i) {
+            str[i] = toupper(str[i]);
+        }
+        if(str==("YES")){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
