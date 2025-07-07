31835d2bb4693683912124798e835661b6b382a1
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-07-07 23:16:45 +0600

    Add files via upload

---
+++ 1996A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int animel=(n/4)+(n%4)/2;
+        cout<<animel<<endl;
+    }
+    return 0;
+}
