bd834f72a7ed0b088d3f5b3c43ca944b7e3db3ba
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-07-09 08:10:32 +0600

    Update 2123B.cpp

--- 2123B.cpp
+++ 2123B.cpp
@@ -1,26 +1,30 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
+ 
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int n,j,k;
-        cin>>n>>j>>k;
-        int arr[n];
-        for(int i=0; i<n; i++){
-            cin>>arr[i];
+    cin >> t;
+ 
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+ 
+        int max_strength = 0;
+        int player_strength;
+ 
+        for (int i = 1; i <= n; ++i) {
+            int x;
+            cin >> x;
+            if (i == m) player_strength = x;
+            max_strength = max(max_strength, x);
         }
-        int count=0;
-        for(int i=0; i<n;i++){
-            if(arr[j-1]<arr[i]){
-                count++;
-            }
-        }
-        if(count<=k){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
+ 
+        if (k > 1 || player_strength == max_strength) {
+            cout << "YES"<<endl;
+        } else {
+            cout << "NO"<<endl;
         }
     }
+ 
     return 0;
 }
