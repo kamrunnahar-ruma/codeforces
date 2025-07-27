1e0f414cc979d7522aa259e18755609a845bf6c4
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-07-27 06:43:40 +0600

    Add files via upload

---
+++ 80A.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+n, m = map(int, input().split())
+
+next_prime = n + 1
+while not is_prime(next_prime):
+    next_prime += 1
+
+if next_prime == m:
+    print("YES")
+else:
+    print("NO")
